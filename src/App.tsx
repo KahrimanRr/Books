770d36d20c6d24bae3164e1d7a47da11c70f6a85
Rijad Kahriman <rijadkahr@gmail.com>
2023-07-04 21:11:35 +0200

    added style for search component not complite

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import { NavBar } from "./layouts/NavbarAndFooter/Navbar";
 
-import { LibraryServices } from "./layouts/HomePage/components/LibraryServices";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { Homepage } from "./layouts/HomePage/Homepage";
 
@@ -11,7 +10,6 @@ export const App = () => {
     <div>
       <NavBar />
       <Homepage />
-      <LibraryServices />
       <Footer />
     </div>
   );
