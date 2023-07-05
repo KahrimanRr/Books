07af073aa245222cb491808396d26f167ce36dc1
Rijad Kahriman <rkahriman27@gmail.com>
2023-07-06 01:46:56 +0200

    added routing dep

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,18 @@ import { NavBar } from "./layouts/NavbarAndFooter/Navbar";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { Homepage } from "./layouts/HomePage/Homepage";
 import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+import { Route } from "react-router-dom";
 
 export const App = () => {
   return (
     <div>
       <NavBar />
-      {/*<Homepage />*/}
-      <SearchBooksPage />
+      <Route path="/">
+        <Homepage />
+      </Route>
+      <Route path="/search">
+        <SearchBooksPage />
+      </Route>
       <Footer />
     </div>
   );
