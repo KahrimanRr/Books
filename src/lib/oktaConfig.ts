aefb27ece5c46542d164d36d865201441bb58970
Rijad Kahriman <rkahriman27@gmail.com>
2023-07-13 02:48:24 +0200

    bug fixed

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
   clientId: "0oaacn00y2pK0bAet5d7",
   issuer: "https://dev-84220665.okta.com/oauth2/default",
-  redirectUri: "https://localhost:3000/login/callback",
+  redirectUri: "http://localhost:3000/login/callback",
   scopes: ["openid", "profile", "email"],
   pkce: true,
   disableHttpsCheck: true,
