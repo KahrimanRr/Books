96ee54cc7d6797f40dcb780e13929c1c81fff3f7
Rijad Kahriman <rkahriman27@gmail.com>
2023-07-13 16:31:07 +0200

    modif pagination

--- src/Utils/Pagination.tsx
+++ src/Utils/Pagination.tsx
@@ -4,31 +4,21 @@ export const Pagination: React.FC<{
   paginate: any;
 }> = (props) => {
   const pageNumbers = [];
-  if (props.currentPage === 1) {
-    pageNumbers.push(props.currentPage);
-    if (props.totalPages >= props.currentPage + 1) {
-      pageNumbers.push(props.currentPage + 1);
-    }
-    if (props.totalPages >= props.currentPage + 2) {
-      pageNumbers.push(props.currentPage + 2);
-    }
-  } else if (props.currentPage > 1) {
-    if (props.currentPage >= 3) {
-      pageNumbers.push(props.currentPage - 2);
-      pageNumbers.push(props.currentPage - 1);
-    } else {
-      pageNumbers.push(props.currentPage - 1);
-    }
-    pageNumbers.push(props.currentPage);
 
-    if (props.totalPages >= props.currentPage + 1) {
-      pageNumbers.push(props.currentPage + 1);
-    }
-    if (props.totalPages >= props.currentPage + 2) {
-      pageNumbers.push(props.currentPage + 2);
-    }
+  if (props.currentPage > 1) {
+    const prevPage = props.currentPage - 1;
+    pageNumbers.push(prevPage);
   }
 
+  pageNumbers.push(props.currentPage);
+
+  if (props.totalPages >= props.currentPage + 1) {
+    pageNumbers.push(props.currentPage + 1);
+  }
+
+  if (props.totalPages >= props.currentPage + 2) {
+    pageNumbers.push(props.currentPage + 2);
+  }
   return (
     <nav aria-label="...">
       <ul className="pagination">
