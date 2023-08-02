5e06d1158cc66dfcded2c65aae728c9add6ba9ca
Rijad Kahriman <rkahriman27@gmail.com>
2023-08-02 21:48:19 +0200

    started creating shelf page

---
+++ src/layouts/ShelfPage/ShelfPage.tsx
@@ -0,0 +1,35 @@
+export const ShelfPage = () => {
+  return (
+    <div className="container">
+      <div className="mt-3">
+        <nav>
+          <div className="nav nav-tabs" id="nav-tab" role="tablist">
+            <button
+              className="nav-link active"
+              id="nav-loans-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#nav-loans"
+              type="button"
+              role="tab"
+              aria-controls="nav-loans"
+              aria-selected="true"
+            >
+              Loans
+            </button>
+            <button
+              className="nav-link"
+              id="nav-history-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#nav-history"
+              type="button"
+              role="tab"
+              aria-controls="nav-history"
+              aria-selected="false"
+            ></button>
+            Your History
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
