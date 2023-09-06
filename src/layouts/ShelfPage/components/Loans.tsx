2f8aae377bfbadc8be36183d36e1465927e9fcb7
Rijad Kahriman <rkahriman27@gmail.com>
2023-09-06 18:06:38 +0200

    modified use effect for current loans

--- src/layouts/ShelfPage/components/Loans.tsx
+++ src/layouts/ShelfPage/components/Loans.tsx
@@ -2,6 +2,7 @@ import { useOktaAuth } from "@okta/okta-react";
 import { useEffect, useState } from "react";
 import ShelfCurrentLoans from "../../../models/ShelfCurrentLoans";
 import { error } from "console";
+import { SpinnerLoading } from "../../../Utils/SpinnerLoading";
 
 export const Loans = () => {
   const { authState } = useOktaAuth();
@@ -14,7 +15,26 @@ export const Loans = () => {
   const [isLoadingUserLoans, setIsLoadingUserLoans] = useState(true);
 
   useEffect(() => {
-    const fetchUserCurrentLoans = async () => {};
+    const fetchUserCurrentLoans = async () => {
+      if (authState && authState.isAuthenticated) {
+        const url = `http://localhost:8080/api/books/secure/currentloans`;
+        const requestOptions = {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+            "Content-Type": "application/json",
+          },
+        };
+        const shelfCurrentLoansResponse = await fetch(url, requestOptions);
+        if (!shelfCurrentLoansResponse.ok) {
+          throw new Error("Something went wrong with current loans");
+        }
+        const shelfCurrentLoansResponseJson =
+          await shelfCurrentLoansResponse.json();
+        setShelfCurrentLoans(shelfCurrentLoansResponseJson);
+      }
+      setIsLoadingUserLoans(false);
+    };
     fetchUserCurrentLoans().catch((error: any) => {
       setIsLoadingUserLoans(false);
       setHttpError(error.message);
@@ -22,5 +42,15 @@ export const Loans = () => {
     window.scrollTo(0, 0);
   }, [authState]);
 
+  if (isLoadingUserLoans) {
+    return <SpinnerLoading />;
+  }
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
   return <></>;
 };
