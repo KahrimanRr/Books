76a3e1a38fc3dfc4df777c71996b17a299ff82ef
Rijad Kahriman <rkahriman27@gmail.com>
2023-09-10 22:28:58 +0200

    removet double q placet single, added state for history

--- src/layouts/ShelfPage/ShelfPage.tsx
+++ src/layouts/ShelfPage/ShelfPage.tsx
@@ -1,11 +1,15 @@
 import { Loans } from "./components/Loans";
+import { HistoryPage } from "./components/HistoryPage";
+import { useState } from "react";
 export const ShelfPage = () => {
+  const [historyClick, setHistoryClick] = useState(false);
   return (
     <div className="container">
       <div className="mt-3">
         <nav>
           <div className="nav nav-tabs" id="nav-tab" role="tablist">
             <button
+              onClick={() => setHistoryClick(false)}
               className="nav-link active"
               id="nav-loans-tab"
               data-bs-toggle="tab"
@@ -18,6 +22,7 @@ export const ShelfPage = () => {
               Loans
             </button>
             <button
+              onClick={() => setHistoryClick(true)}
               className="nav-link"
               id="nav-history-tab"
               data-bs-toggle="tab"
@@ -34,7 +39,7 @@ export const ShelfPage = () => {
         <div className="tab-content" id="nav-tabContent">
           <div
             className="tab-pane fade show active"
-            id=" nav-loans"
+            id="nav-loans"
             role="tabpanel"
             aria-labelledby="nav-loans-tab"
           >
@@ -44,9 +49,9 @@ export const ShelfPage = () => {
             className="tab-pane fade"
             id="nav-history"
             role="tabpanel"
-            aria-labelledby="nav-history-tav"
+            aria-labelledby="nav-history-tab"
           >
-            <p>Checkout History </p>
+            {historyClick ? <HistoryPage /> : <></>}
           </div>
         </div>
       </div>
